9ed6ecb16a987817df74d8ccceef5667614d70ff
Souheil CHELFOUH <trollfot@gmail.com>
2010-07-06 18:24:46 +0200

    corrected import

--- src/menhir/contenttype/folder/views.py
+++ src/menhir/contenttype/folder/views.py
@@ -6,7 +6,6 @@ from dolmen import menu
 from dolmen.app.container import listing
 from dolmen.app.layout import Page
 from dolmen.app.viewselector import SelectableViewsMenu
-from megrok.z3cform.base import IGrokForm
 from menhir.contenttype.folder import IFolder
 from zope.component import getMultiAdapter
 
@@ -28,5 +27,4 @@ class CompositeView(Page):
         for item in self.context.values():
             view = getMultiAdapter((item, self.request), name="index")
             view.update()
-            if IGrokForm.providedBy(view): view.updateForm()
             self.items.append(view.content())
